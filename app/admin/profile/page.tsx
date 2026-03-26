04540e3fe6f3c35a2e4f35c8c0faf64369793c53
Ederson <jvedrson@gmail.com>
2026-03-25 23:11:55 -0400

    Add new pages for product administration, user profile, login, registration, checkout, shopping cart, and user editing

---
+++ app/admin/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+      <main className="flex min-h-screen flex-col p-6">
+        <h1 className="text-2xl font-bold mb-4">Admin Profile</h1>
+        <p>This is the admin profile page.</p>
+      </main>
+  );
+}
