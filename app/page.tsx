65a97ba40d47a0cdfeb7ab1b9b0f2f16277f6ad0
Jared Samayoa <jsamayoapenagos@byupathway.edu>
2026-04-03 14:36:02 -0600

    Connected the app to the postgres DB. Landing page is now pulling the featured products from the DB.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import HhLogo from '@/app/ui/hh-logo';
 import { merriweather, roboto } from '@/app/ui/fonts';
+import { getProducts } from '@/app/lib/data/product.data';
 
+export default async function Page() {
+  const products = await getProducts();
 
-export default function Page() {
   return (
     <main className="flex flex-grow flex-col p-0">
 
@@ -15,26 +17,26 @@ export default function Page() {
         </a>
       </section>
 
-      {/* Featured Products */}
+     {/* Featured Products */}
       <section className="py-16 px-8">
         <h2 className="text-3xl font-bold text-center mb-10">Featured Products</h2>
 
         {/* Responsive grid with tighter spacing */}
         <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6 justify-center">
-          {[1, 2, 3, 4, 5, 6].map((item) => (
+          {products.map((product: any) => (
             <div
-              key={item}
+              key={product.id}
               className="bg-white rounded-lg shadow p-6 text-center max-w-[250px] mx-auto 
                    transform transition duration-300 hover:scale-105 hover:shadow-lg"
             >
               <img
-                src={`https://placehold.co/200x200`}
-                alt={`Product ${item}`}
+                src={product.image || `https://placehold.co/200x200`}
+                alt={product.name}
                 className="w-full aspect-square object-cover rounded transition duration-300 hover:opacity-90"
               />
-              <h3 className="mt-4 font-semibold">Product {item}</h3>
-              <p>Seller Name</p>
-              <p className="text-gray-600">$19.99</p>
+              <h3 className="mt-4 font-semibold">{product.name}</h3>
+              <p>{product.seller_name}</p>
+              <p className="text-gray-600">${product.price}</p>
               <button className="mt-4 px-4 py-2 bg-hhblue-400 text-white rounded 
                            hover:bg-hhorange-600 transition duration-300">
                 Add to Cart
